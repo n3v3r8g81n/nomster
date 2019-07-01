ee5cee170a280ce7fbf0cc5ae8d939afd367c5e9
Laur dArmien <n3v3r8g81n@gmail.com>
2019-07-01 20:18:08 +0000

    fixed migration error

--- db/migrate/20190701175720_create_places.rb
+++ db/migrate/20190701175720_create_places.rb
@@ -2,7 +2,7 @@ class CreatePlaces < ActiveRecord::Migration[5.2]
   def change
     create_table :places do |t|
       t.string :name
-      t.address
+      t.string :address
       t.text :description
       t.timestamps
     end

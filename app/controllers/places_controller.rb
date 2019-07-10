4a83bade7463fd9b5ca9891a229e9ab8fd28e0fd
Laur dArmien <n3v3r8g81n@gmail.com>
2019-07-10 01:23:19 +0000

    Lesson 4 done

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,20 @@
 class PlacesController < ApplicationController
     def index
- #     @places = Place.all
+      @places = Place.paginate(page: params[:page])
  # Item.paginate page: params[:page], per_page: 10”
-        @places = Place.order(:id).paginate(:page => params[:page], :per_page =>3)
+ #       @places = Place.order(:id).paginate(:page => params[:page], :per_page =>3)
+      end
+
+    def new
+      @place = Place.new
+      end
+
+    def add
+      Place.create(place_params)
+      redirect_to root_path
+      end
+    private
+    def place_params
+      params.require(:place).permit(:name, :description, :address)
       end
 end

3a42e6b755f1fb7a0838d438467374f2b647a000
Laur dArmien <n3v3r8g81n@gmail.com>
2019-07-15 19:02:57 +0000

    periodic checkin

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
     def index
       @places = Place.paginate(page: params[:page])
  # Item.paginate page: params[:page], per_page: 10”
- #       @places = Place.order(:id).paginate(:page => params[:page], :per_page =>3)
+ # @places = Place.order(:id).paginate(:page => params[:page], :per_page =>3)
       end
 
     def new

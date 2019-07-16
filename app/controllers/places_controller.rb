055ca66a21af2ac29c027c7a810bf0d85f7b52f4
Laur dArmien <n3v3r8g81n@gmail.com>
2019-07-16 00:11:46 +0000

    lesson 23 done

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,21 +1,33 @@
 class PlacesController < ApplicationController
+    # added in lesson  22 to match lesson code
+    before_action :authenticate_user!, only: [:new, :create]
+
     def index
-      @places = Place.paginate(page: params[:page])
- # Item.paginate page: params[:page], per_page: 10”
- # @places = Place.order(:id).paginate(:page => params[:page], :per_page =>3)
+      @places = Place.order(:id).paginate(:page => params[:page], :per_page =>3)
       end
 
     def new
       @place = Place.new
       end
 
-    def add
+    def create
       current_user.places.create(place_params)
       redirect_to root_path
       end
 
     def show
       @place = Place.find(params[:id])
+    end
+
+    def edit
+      @place = Place.find(params[:id])
+      
+    end
+
+    def update
+      @place = Place.find(params[:id])
+      @place.update_attributes(place_params)
+      redirect_to root_path
     end
 
     private

6b6d1282d54dd05db1908c1567e2dbcd8a663b85
Laur dArmien <n3v3r8g81n@gmail.com>
2019-07-17 01:01:53 +0000

    lesson 25 done

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
     # added in lesson  22 to match lesson code
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
     def index
       @places = Place.order(:id).paginate(:page => params[:page], :per_page =>3)
@@ -21,11 +21,16 @@ class PlacesController < ApplicationController
 
     def edit
       @place = Place.find(params[:id])
-      
+      if @place.user !=current_user
+        return render plain: "You do not have permission to edit this place.", status: :forbidden
+      end
     end
 
     def update
       @place = Place.find(params[:id])
+      if @place.user != current user
+        return render plain: "You do not have permission to edit this place.", status: :forbidden
+      end
       @place.update_attributes(place_params)
       redirect_to root_path
     end
@@ -33,7 +38,10 @@ class PlacesController < ApplicationController
     # added in lesson 23
     def destroy
       @place = Place.find(params[:id])
-      @place.destroy
+      if @place.user != current_user
+        return render plain: "You do not have permission to edit this place.", status: :forbidden
+      end
+        @place.destroy
       redirect_to root_path
 
     end

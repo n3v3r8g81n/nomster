5d949926cb057b1678ef3c4de34f1b2bd70b09e7
Laur dArmien <n3v3r8g81n@gmail.com>
2019-07-18 20:23:13 +0000

    lesson 30 first push

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,8 @@
 class Place < ApplicationRecord
 #  self.per_page = 3
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
   validates :name, presence: true, length: { minimum: 3 }
   validates :address, presence: true
   validates :description, presence: true

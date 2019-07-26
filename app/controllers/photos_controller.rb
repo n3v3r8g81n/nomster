911b85694d001e9de1bceda1558e11b191697a58
Laur dArmien <n3v3r8g81n@gmail.com>
2019-07-26 21:55:22 +0000

    multiple changes to fix previous error. Error is now showing photos.controller, ln 6

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,8 +3,9 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    @place.photos.create(photo_params.merge(user: current_user))
-    redirect_to '#'
+    @place.photos.create(photo_params) redirect_to place_path(@place)
+#@place.photos.create(photo_params.merge(user: current_user))
+    # redirect_to '#'
     # redirect_to 'place_photos_path(@photo)'
   end
 

ebdc10648e31d4e221b6b591f866f903c13b2b7f
Laur dArmien <n3v3r8g81n@gmail.com>
2019-07-26 19:56:39 +0000

    moved devise_for users up to top

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
   root 'places#index'
   resources :places do
     resources :comments, only: :create
-    #resources :photos, only: :create
+    resources :photos, only: :create
   end
-  devise_for :users
 
 end

f623840f31cb2f89adf8dbe6a065258b2e3ff2c5
Laur dArmien <n3v3r8g81n@gmail.com>
2019-07-27 23:45:17 +0000

    fixed cancel button error

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,13 +12,13 @@ class PlacesController < ApplicationController
 
     def create
       @place = current_user.places.create(place_params)
-      redirect_to root_path
+      # redirect_to root_path
       if @place.valid?  
         redirect_to root_path
       else
         render :new, status: :unprocessable_entity
       end 
-      end
+    end
 
     def show
       @place = Place.find(params[:id])

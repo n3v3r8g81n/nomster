875274b33c5e62d663cd4df9544dc94ecd901ec8
Laur dArmien <n3v3r8g81n@gmail.com>
2019-08-17 20:39:26 +0000

    lesson 43 done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,16 +34,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   # Adding mailer from lesson 16
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "example.com",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: ENV['GMAIL_ADDRESS'],
+  #   password: ENV['GMAIL_PASSWORD']
+  # }
   config.action_mailer.default_url_options = { host: 'localhost:3030'}
   config.action_mailer.perform_caching = false
 
